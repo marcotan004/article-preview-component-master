c6e78cf0c1aec7c8d541a6613cc54e18666a579e
marcotan004 <marcotan004@gmail.com>
2026-03-22 13:22:29 -0400

    accessibility and anchor fixes

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
+const shareContainer = document.getElementById("share-container");
 const footerDisplay = document.getElementById("footer-display");
 
 const toggleShare = (e) => {
     if (footerDisplay.classList.contains("share-open")) {
         footerDisplay.classList.remove("share-open");
+        shareContainer.setAttribute("aria-expanded", "false");
 
     } else {
         footerDisplay.classList.add("share-open");
+        shareContainer.setAttribute("aria-expanded", "true");
     }
 }
 
