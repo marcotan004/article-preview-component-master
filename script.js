9658de76f0a9d41b384ccf96a00c54ee0190b738
marcotan004 <marcotan004@gmail.com>
2026-03-22 07:41:10 -0400

    simplify css/js

--- script.js
+++ script.js
@@ -2,18 +2,17 @@ const shareTooltip = document.getElementById("share-tooltip");
 const profile = document.getElementById("profile");
 const shareContainer = document.getElementById("share-container");
 const footerDisplay = document.getElementById("footer-display");
+const shareRibbon = document.getElementById("share-ribbon");
 
 const toggleShare = (e) => {
     if (profile.classList.contains("hidden")) {
         profile.classList.remove("hidden");
         shareTooltip.classList.add("hidden");
-        footerDisplay.classList.remove("footer-display-share");
-        footerDisplay.classList.add("footer-display-profile");
+        footerDisplay.classList.remove("dark");
     } else {
-        profile.classList.add("hidden");
         shareTooltip.classList.remove("hidden");
-        footerDisplay.classList.add("footer-display-share");
-        footerDisplay.classList.remove("footer-display-profile");
+        profile.classList.add("hidden");
+        footerDisplay.classList.add("dark");
     }
     
 }
